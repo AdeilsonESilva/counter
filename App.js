b047d1c21aa95050f3c467b892936b96cb16ab79
Lucas Augusto <lucasaugustscode@gmail.com>
2021-12-29 17:45:41 -0300

    feat: Project creation code base.

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+
+export const App = () => {
+  return (
+    <View>
+      <Text>Counter</Text>
+    </View>
+  )
+}
+
+export default App
