6cc58940ef0d30dc22a156a1f7423edb34bc4b29
Lucas Augusto <lucasaugustscode@gmail.com>
2021-12-29 18:18:59 -0300

    chore: Addition of the path alias in the project.

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,25 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ts', '.tsx', '.jsx', '.js', '.json'],
+        alias: {
+          '@routes': './src/routes',
+          '@screens': './src/screens',
+          '@components': './src/components',
+          '@libs': './src/libs',
+          '@contexts': './src/contexts',
+          '@dtos': './src/dtos',
+          '@services': './src/services',
+          '@hooks': './src/hooks',
+          '@styles': './src/styles',
+          '@assets': './src/assets',
+          '@utils': './src/utils'
+        }
+      }
+    ]
+  ]
 }
