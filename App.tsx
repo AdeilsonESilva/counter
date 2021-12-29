603f85d003f6d33f1e538c4612a4b4c06646cb41
Lucas Augusto <lucasaugustscode@gmail.com>
2021-12-29 18:00:22 -0300

    feat: Addition of TypesScript to the project.

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { View, Text } from 'react-native'
 
 
-export const App = () => {
+export const App: React.FC = () => {
   return (
     <View>
       <Text>Counter</Text>
