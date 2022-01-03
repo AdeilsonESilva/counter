b7d7ee7d32dab660672b53c1b8d93ee45081cba4
Lucas Augusto <lucasaugustscode@gmail.com>
2022-01-03 00:24:47 -0300

    feat: New colors.

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,12 +1,13 @@
 export const theme = {
   COLORS: {
     main: '#FF533C',
-    main_light: 'rgba(255, 83, 60, 0.2)',
+    main_light: 'rgba(255, 83, 60, 0.1)',
 
     title: '#161616',
     description: '#5C5C5C',
 
     border: '#EEEEEE',
+    border_light: '#EFEFEF',
     shape: '#FCFCFC',
 
     icon: '#7D7D7D',
