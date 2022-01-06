70bf03ccd9bed5017517e67ada79914d3172af22
Lucas Augusto <lucasaugustscode@gmail.com>
2022-01-05 22:13:37 -0300

    feat: Define the theme structure typing.

---
+++ src/dtos/theme.ts
@@ -0,0 +1,29 @@
+export interface ITheme {
+  NAME: string
+
+  COLORS: {
+    main: string
+    main_light: string
+
+    success: string
+    success_light: string
+
+    title: string
+    description: string
+
+    border: string
+    border_light: string
+    shape: string
+
+    icon: string
+
+    opacity_dark: string
+    background: string
+  }
+  FONTS: {
+    primary_semiBold: string
+    primary_bold: string
+
+    secondary_medium: string
+  }
+}
